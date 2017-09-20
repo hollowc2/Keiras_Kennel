eead7256a3417604cf5838349ad3cbe9f9d8621f
corey <hollowc2@gmail.com>
2017-09-19 19:38:45 -0700

    v0.0.1

--- Twitter_Bot/src/server/twit_server.py
+++ Twitter_Bot/src/server/twit_server.py
@@ -11,35 +11,9 @@ def twitter_api():
     api = tweepy.API(auth)
     return api
 
-
 camera = picamera.PiCamera()
-
 camera.capture('image.jpg')
-
-
-
-
-
-# def tweet_image(url, message):
-#     api = twitter_api()
-#     filename = 'image.jpg'
-#     request = requests.get(url, stream=True)
-#     if requests.status_code == 200:
-#         with open(filename, 'wb') as image:
-#             for chunk in request:
-#                 image.write(chunk)
-#
-#         api.update_with_media(filename, status=message)
-#         os.remove(filename)
-#     else:
-#         print("Unalbe to download image")
-#
-# url = 'image.jpg'
-# message = "Cheese"
-# tweet_image(url,message)
 api = twitter_api()
-
-
 api.update_with_media('image.jpg', status="Heres my face")
 print 'took a pic'
 

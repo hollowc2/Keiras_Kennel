fe0d77a55377c89d98d7bb96f90c693a38fc1362
corey <hollowc2@gmail.com>
2017-09-19 19:25:32 -0700

    Test Tweet  with pic

--- Twitter_Bot/src/server/twit_server.py
+++ Twitter_Bot/src/server/twit_server.py
@@ -1,17 +1,44 @@
 import picamera
 import tweepy
+import requests
+import os
 from credentials import *
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+def twitter_api():
 
-api = tweepy.API(auth)
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    return api
+
+
 camera = picamera.PiCamera()
 
 camera.capture('image.jpg')
 
 
-api.update_status(status="Keira loves to eat turds", image='image.jpg')
+
+
+
+def tweet_image(url, message):
+    api = twitter_api()
+    filename = 'image.jpg'
+    request = requests.get(url, stream=True)
+    if requests.status_code == 200:
+        with open(filename, 'wb') as image:
+            for chunk in request:
+                image.write(chunk)
+
+        api.update_with_media(filename, status=message)
+        os.remove(filename)
+    else:
+        print("Unalbe to download image")
+
+url = 'image.jpg'
+message = "Cheese"
+tweet_image(url)
+
+
 
 print 'took a pic'
 
